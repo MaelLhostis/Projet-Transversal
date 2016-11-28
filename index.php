77d4b33bf057c72b1fe2f7952b7c23e6668f68f2
kerweran <aubryboucard@gmail.com>
2016-11-28 10:04:03 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
 <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css" />
 </head>
 <body>
+<h2>Welcome, <?php if (isset($_GET["name"])) echo $_GET["name"];
+?></h2>
 <div class="w3-container w3-third"></div>
 <div class="w3-container w3-third">Contenu formulaire</div>
 <div class="w3-container w3-third"></div>
