68eb1f69cb32ee758ab00afa502a61b5d094eca4
kerweran <aubryboucard@gmail.com>
2016-11-30 11:08:32 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,17 +1,47 @@
-// index.php
 <!DOCTYPE html>
-<html>
+<html lang="fr">
 <head>
-<title>TP GIT</title>
-<?php session_start(); ?>
-<meta charset="UTF-8" />
-<link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css" />
+	<meta charset="utf-8">
+	<title>Accueil</title>
+	<link rel="stylesheet" type="text/css" href="w3.css">
 </head>
 <body>
-<h2>Welcome, <?php if (isset($_GET["name"])) echo $_GET["name"];
-?></h2>
-<div class="w3-container w3-third"></div>
-<div class="w3-container w3-third">Contenu formulaire</div>
-<div class="w3-container w3-third"></div>
+	<header>
+		<h2>Chantal RIEN Consultant</h2>
+		<h3>Activité Conseil et de Prévention Santé</h3>
+		<nav>
+			<ul class="w3-pagination">
+				<li><a href="">Accueil</a></li>
+				<li>Mes Services
+					<ul>
+						<li><a href="">Structures Sociales et Sanitaires</a></li>
+						<li><a href="">Lieux d'Accueil de Jeunes</a></li>
+						<li><a href="">Entreprises</a></li>
+					</ul>
+				</li>
+				<li><a href="">Contacts</a></li>
+			</ul>
+		</nav>
+	</header>
+	<aside id="contactsAside">
+		<h3>Contacts</h3>
+		<ul>
+			<li>Téléphone</li>
+			<li>Mail</li>
+		</ul>
+	</aside>
+	<section>
+		<q>Qualité, Prestation personnalisée, Proximité.</q>
+		<article>
+			<p>Conseil et Prévention Santé pour les structures sociales et sanitaires, les lieux d'accueil de jeunes et les entreprises </p>
+		</article>
+		<article>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nibh velit, viverra nec congue ac, mollis quis arcu. Sed euismod ligula mauris, a egestas risus ultrices quis. Nunc mollis diam sapien, aliquam dapibus massa vulputate at. Sed tristique leo sit amet cursus sollicitudin. Sed lectus nulla, molestie bibendum convallis et, egestas in dui. Vivamus venenatis mi eu libero finibus, fermentum efficitur nulla volutpat. Ut eu tellus tortor. Integer eu interdum massa. Quisque quis nisl in turpis aliquet pellentesque. Cras consectetur lectus non urna placerat hendrerit. Donec at neque mattis, ornare lectus non, volutpat quam. Sed luctus est ac diam ullamcorper vulputate. Nunc commodo leo eu ante laoreet, sit amet lacinia tortor iaculis. Phasellus fermentum auctor ex nec condimentum. Cras porttitor nisl in risus mattis rhoncus et eu risus.</br></br>
+			Vivamus pharetra scelerisque lorem ac vestibulum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec mollis condimentum metus, venenatis suscipit lectus volutpat sit amet. Nunc lobortis porta nunc in sodales. Suspendisse potenti. Duis quis eros dolor. Etiam mi nisl, aliquet sed sem eget, laoreet aliquam eros. Donec eget lectus vel eros finibus auctor. Nunc vel nisl nibh.
+		</article>
+	</section>
+	<footer>
+		
+	</footer>
 </body>
 </html>
